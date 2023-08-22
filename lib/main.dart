d29f4566c151a9419b3cf862569b464272772458
Rinaldi Andika <rinaldi0121@gmail.com>
2023-08-22 17:46:51 +0700

    my first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'second.dart';
+import 'hotel_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// myapp stateless
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reservasi Hotel',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+// myhomepage using stateless widget
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Reservasi Hotel'),
+      ),
+      body: const HotelMobileTemplateCard(),
+    );
+  }
+}
+
+// hotel card stateless
+class HotelMobileTemplateCard extends StatelessWidget {
+  const HotelMobileTemplateCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // item count data length
+      itemCount: hotelDataList.length,
+      itemBuilder: (context, index) {
+        final HotelData hotel = hotelDataList[index];
+        return Container(
+          height: 350,
+          padding: const EdgeInsets.all(5),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 215,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      // data path image
+                      hotel.imageListAsset,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 15, right: 10),
+                  child: SizedBox(
+                    height: 114,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  // data namaHotel
+                                  hotel.namaHotel,
+                                  textAlign: TextAlign.left,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  // data kota
+                                  hotel.kota,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  // data jumlah pengunjung
+                                  '${hotel.jumlahPengunjung} Pengunjung',
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                const SizedBox(width: 45),
+                                const IconTheme(
+                                  data: IconThemeData(
+                                    color: Colors.amber,
+                                    size: 20,
+                                  ),
+                                  child: Icon(Icons.star_rate),
+                                ),
+                                Text(
+                                  // data rating
+                                  hotel.rating,
+                                  style: const TextStyle(fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const FavouriteButton(),
+                            const SizedBox(width: 15),
+                            // parameter diisikan widget untuk navigasi
+                            NavIconForward(hotel: hotel),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+// navigation icon forward class
+class NavIconForward extends StatelessWidget {
+  const NavIconForward({Key? key, required this.hotel}) : super(key: key);
+  final HotelData hotel;
+// inisialisasikan index dari list class kedalam parameter
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: Colors.amber,
+      child: IconButton(
+        icon: const Icon(Icons.arrow_forward_outlined, color: Colors.white),
+        //navigasi disini
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return HotelDetails(hotel: hotel);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// icon favourite stateless
+class FavouriteButton extends StatefulWidget {
+  const FavouriteButton({Key? key}) : super(key: key);
+
+  @override
+  State<FavouriteButton> createState() => _FavouriteButtonState();
+}
+
+class _FavouriteButtonState extends State<FavouriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+        size: 35,
+      ),
+      onPressed: () {
+        setState(
+          () {
+            isFavorite = !isFavorite;
+          },
+        );
+      },
+    );
+  }
+}
---
+++ lib/second.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'hotel_data.dart';
+
+class HotelDetails extends StatelessWidget {
+  const HotelDetails({Key? key, required this.hotel}) : super(key: key);
+  final HotelData hotel;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Deskripsi Hotel'),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.arrow_back_ios_new),
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Carousel(
+                detailImagePath: hotel.detailImageHotelList,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: HotelInfo(
+                name: hotel.namaHotel,
+                rating: hotel.rating,
+                location: hotel.lokasi,
+              ),
+            ),
+            const TabBar(
+              tabs: [
+                Tab(text: 'Deskripsi'),
+                Tab(text: 'Benefit'),
+                Tab(text: 'Alamat'),
+                Tab(text: 'Ulasan'),
+              ],
+            ),
+            SizedBox(
+              height: 302,
+              child: TabBarView(
+                children: [
+                  Text(hotel.deskripsiTabHotel),
+                  Text(hotel.benefitTabHotel),
+                  Text(hotel.alamatTabHotel),
+                  Text(hotel.ulasanTabHotel),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class for hotel info
+class HotelInfo extends StatelessWidget {
+  const HotelInfo({
+    Key? key,
+    required this.name,
+    required this.rating,
+    required this.location,
+  }) : super(key: key);
+
+  final String location;
+  final String name;
+  final String rating;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          name,
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            const IconTheme(
+              data: IconThemeData(color: Colors.amber, size: 20),
+              child: Icon(Icons.star_rate),
+            ),
+            Text(rating, style: const TextStyle(fontSize: 15)),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            const IconTheme(
+              data: IconThemeData(color: Colors.grey, size: 20),
+              child: Icon(Icons.location_on_outlined),
+            ),
+            Text(location, style: const TextStyle(fontSize: 15)),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// class for carousel(image slider with animation)
+class Carousel extends StatefulWidget {
+  const Carousel({Key? key, required this.detailImagePath}) : super(key: key);
+
+  final List<String> detailImagePath;
+
+  @override
+  State<Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 250.0,
+            enlargeCenterPage: true,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 10),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+          items: List.generate(widget.detailImagePath.length, (index) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: AssetImage(widget.detailImagePath[index]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
+        ),
+        const SizedBox(height: 15.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.detailImagePath.length, (index) {
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentIndex == index ? Colors.amber : Colors.grey,
+              ),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+}
